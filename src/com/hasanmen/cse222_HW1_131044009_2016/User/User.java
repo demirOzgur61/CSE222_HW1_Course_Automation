bd3b3a791907b16b67bf00ecd3a75f5b1cb463c4
hmenn <hmen@gyte.edu.tr>
2016-02-21 02:35:08 +0200

    Added user

--- src/com/hasanmen/cse222_HW1_131044009_2016/User/User.java
+++ src/com/hasanmen/cse222_HW1_131044009_2016/User/User.java
@@ -1,25 +1,60 @@
 package com.hasanmen.cse222_HW1_131044009_2016.User;
 
+import com.hasanmen.cse222_HW1_131044009_2016.Course.Course;
+
+import java.util.List;
+
 /**
  * Created by Hasan MEN on 20.02.2016.
  */
 public abstract class User {
 
-    protected static int USER_ID = 0;
-    private String name;
-    private String surName;
-    private String userName;
-    private String eMail;
-    private String passWord;
+    protected static int TOTAL_USER_ID = 0;
+    private List<Course> accessibleCourses = null; // erisileblinen kurslar
+    private List<User> accessibleUsers = null;
+    private String name = null;
+    private String surName = null;
+    private String userName = null;
+    private String eMail = null;
+    private String passWord = null;
     private int userID;
 
-    public User(int userID, String name, String surName, String userName, String passWord, String eMail) {
+    public User(String name, String surName, String userName, String passWord, String eMail) {
         this.name = name;
         this.surName = surName;
         this.userName = userName;
         this.eMail = eMail;
         this.passWord = passWord;
-        this.userID = userID;
+        this.userID = TOTAL_USER_ID;
+        ++TOTAL_USER_ID;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getSurName() {
+        return surName;
+    }
+
+    public String geteMail() {
+        return eMail;
+    }
+
+    public List<Course> getAccessibleCourses() {
+        return accessibleCourses;
+    }
+
+    public List<User> getAccessibleUsers() {
+        return accessibleUsers;
+    }
+
+    public void setAccessibleCourses(List<Course> accessibleCourses) {
+        this.accessibleCourses = accessibleCourses;
+    }
+
+    public void setAccessibleUsers(List<User> accessibleUsers) {
+        this.accessibleUsers = accessibleUsers;
     }
 
     public String getUserName() {
@@ -30,8 +65,13 @@ public abstract class User {
         return passWord;
     }
 
+    public int getUserID() {
+        return userID;
+    }
+
     /**
      * Gelen obje ile USERNAME VE PASSWORD ayni ise true return  edecek.Diger durumlarda false return eder
+     *
      * @param obj Karsilastirilacak USER INSTACE i olmali
      * @return Esitlik durumunu return eder
      */
