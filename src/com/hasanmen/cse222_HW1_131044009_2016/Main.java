96f933255f2af704cff9f467b359cb844bfb5168
hmenn <hmen@gyte.edu.tr>
2016-02-21 02:34:25 +0200

    Added some test

--- src/com/hasanmen/cse222_HW1_131044009_2016/Main.java
+++ src/com/hasanmen/cse222_HW1_131044009_2016/Main.java
@@ -1,11 +1,38 @@
 package com.hasanmen.cse222_HW1_131044009_2016;
 
 
+import com.hasanmen.cse222_HW1_131044009_2016.Course.Course;
+import com.hasanmen.cse222_HW1_131044009_2016.User.Admin;
+import com.hasanmen.cse222_HW1_131044009_2016.User.Teacher;
+import com.hasanmen.cse222_HW1_131044009_2016.User.User;
 
 public class Main {
     public static void main(String... args){
+        Admin admin=null;
+        User user=null;
+
 
         AutomationSystem GTUStudentSystem = new AutomationSystem("Gebze Technical University Moodle System");
+        User loginUser = GTUStudentSystem.login("hmenn","hasan5669");
+
+
+        if(loginUser instanceof Admin ){
+            admin = (Admin) loginUser;
+        }
+
+        admin.addCourse(new Course("CSE222"));
+
+        admin.getAccessibleCourses().get(0).getCourseTeachers().add(new Teacher("Fatih Erdogan","Sevilgen","ferdogansev","123","ferdogan@gtu.edu.tr"));
+        admin.getAccessibleCourses().get(0).getCourseTeachers().add(new Teacher("Fatih Erdogan","Sevilgen","ferdogansev","123","ferdogan@gtu.edu.tr"));
+        System.out.println(admin.getAccessibleCourses().get(0));
+
+        System.out.println(GTUStudentSystem);
+
+
+
+
+
+
 
     }
 }
